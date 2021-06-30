dfbb269ec46cd60fb79a7eaa115ddfd9145c5789
Uzzipro <ubassi15@gmail.com>
2021-06-30 20:09:29 +0530

    untracked files added

---
+++ app/src/main/java/com/canatme/zpirit/Utils/Constants.java
@@ -0,0 +1,8 @@
+package com.canatme.zpirit.Utils;
+
+public class Constants {
+    /* Fragment names */
+    public static final String LOGIN_FRAGMENT = "LOGIN_FRAGMENT";
+    public static final String SIGNUP_FRAGMENT = "SIGNUP_FRAGMENT";
+
+}
