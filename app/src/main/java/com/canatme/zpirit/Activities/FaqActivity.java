70bfa26dcbab69f0df6db32abfb70ed56b166a93
Uzzipro <ubassi15@gmail.com>
2021-07-21 07:17:51 +0530

    loading bar added

--- app/src/main/java/com/canatme/zpirit/Activities/FaqActivity.java
+++ app/src/main/java/com/canatme/zpirit/Activities/FaqActivity.java
@@ -1,16 +1,24 @@
 package com.canatme.zpirit.Activities;
 
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ExpandableListView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.canatme.zpirit.Adapters.CartAdapter;
+import com.airbnb.lottie.LottieAnimationView;
 import com.canatme.zpirit.Adapters.FaqAdapter;
 import com.canatme.zpirit.Dataclasses.FaqDto;
 import com.canatme.zpirit.R;
@@ -23,7 +31,6 @@ import com.google.firebase.database.ValueEventListener;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class FaqActivity extends AppCompatActivity {
@@ -32,6 +39,7 @@ public class FaqActivity extends AppCompatActivity {
     private DatabaseReference getFaqs;
     private RecyclerView rvFaqs;
     private List<FaqDto> faqList;
+    private AlertDialog loadingDialog;
     private FaqAdapter adapter;
 
     @Override
@@ -47,10 +55,7 @@ public class FaqActivity extends AppCompatActivity {
         int numberOfColumns = 1;
         rvFaqs.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
         rvFaqs.setAdapter(adapter);
-
-
-
-
+        loadingScreen();
         getFaqs();
     }
 
@@ -59,15 +64,17 @@ public class FaqActivity extends AppCompatActivity {
         getFaqs.child("faqs").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                if(snapshot.hasChildren())
-                {
-                    for(DataSnapshot dataSnapshot : snapshot.getChildren())
-                    {
+                if (snapshot.hasChildren()) {
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                         FaqDto faqDto = dataSnapshot.getValue(FaqDto.class);
-                        Log.e(TAG, "onDataChange: "+faqDto.getQuestion());
+                        Log.e(TAG, "onDataChange: " + faqDto.getQuestion());
                         faqList.add(faqDto);
                         adapter.notifyDataSetChanged();
                     }
+                    loadingDialog.dismiss();
+                } else {
+                    loadingDialog.dismiss();
+
                 }
 
             }
@@ -77,6 +84,26 @@ public class FaqActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    private void loadingScreen() {
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View dialogLoading = factory.inflate(R.layout.loading, null);
+        loadingDialog = new AlertDialog.Builder(this).create();
+        Window window = loadingDialog.getWindow();
+        WindowManager.LayoutParams wlp = window.getAttributes();
+        wlp.gravity = Gravity.CENTER;
+//        wlp.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        window.setAttributes(wlp);
+        loadingDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        loadingDialog.setCancelable(false);
+        loadingDialog.setView(dialogLoading);
+        loadingDialog.show();
+        TextView tvLoading = dialogLoading.findViewById(R.id.tvLoading);
+        LottieAnimationView animation_view = dialogLoading.findViewById(R.id.animation_view);
+        tvLoading.setOnClickListener(view -> {
+            animation_view.cancelAnimation();
+            loadingDialog.dismiss();
+        });
     }
 }
